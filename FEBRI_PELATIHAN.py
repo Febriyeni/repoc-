3c81b44a9a88c9a9d8f334f98f0b1161226e4cf4
Febriyeni <yenifebri623@gmail.com>
2024-10-09 18:28:10 -0700

    file pyton
    ini pertama kali ngpush di github

---
+++ FEBRI_PELATIHAN.py
@@ -0,0 +1,26 @@
+Python 3.8.2 (tags/v3.8.2:7b3ab59, Feb 25 2020, 23:03:10) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+============== RESTART: C:/Users/LENUVO/Documents/BIODATA_FEBRI.py =============
+=BIODATA=
+masukkan nama=Nuruul Hasanah Febriani
+tempat lahir=gelogor mapong
+tanggal lahir=08,02,2006
+alamat=Praya
+umur=18
+input gender=perempuan
+sekolah dasar=MI NURUL ITTIHAD/2013-2018
+sekolah menengah pertama=MTs NURUL ITTIHAD/2018-2021
+sekolah menengah atas=MAN 1 LOMBOK TENGAH/2021-2024
+===========================
+nama :Nurul Hasanah Febriani
+tempat lahir :gelogor mappong
+tanggal lahir :08,02,2006
+alamt :Praya
+umur :18
+input gender :perempuan
+sekolah dasar :MI NURUL ITTIHAD/2013-2018
+sekolah menengah pertama :MTs NURUL ITTIHAD/2018-2021
+sekolah menengah atas :MAN 1 LOMBOK TENGAH/2021-2024
+===========================
+>>> 
